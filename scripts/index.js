d69cc1bc9ce244f5d354bcd61624ce681fdf0f2a
lordprk <119014531+lordprk@users.noreply.github.com>
2023-02-05 03:12:26 +0700

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,26 @@
-let likeButtons = document.querySelectorAll(".place__button-like");
-
-for (let i = 0; i < likeButtons.length; i++) {
-  likeButtons[i].addEventListener("click", function () {
-    likeButtons[i].classList.toggle("place__button-like_active");
-  });
-}
-
+//Переменные
 let popupElement = document.querySelector(".popup");
 let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input_name");
-let jobInput = document.querySelector(".popup__input_caption");
+let nameInput = document.querySelector(".popup__input_name_profile_name");
+let jobInput = document.querySelector(".popup__input_name_profile_caption");
 let profileName = document.querySelector(".profile__title");
 let profileJob = document.querySelector(".profile__subtitle");
 let popupOpenButton = document.querySelector(".profile__edit-button");
 let popupCloseButton = document.querySelector(".popup__button-close");
 
+//Функция открытия popup
 let popupOpen = function () {
   popupElement.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 };
-popupOpenButton.addEventListener("click", popupOpen);
 
+//Функция закрытия popup  
 let popupClose = function () {
   popupElement.classList.remove("popup_opened");
 };
-popupCloseButton.addEventListener("click", popupClose);
 
+//Функция отправки формы
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -34,4 +28,16 @@ function handleFormSubmit(evt) {
   popupClose();
 }
 
+//Результат
+popupOpenButton.addEventListener("click", popupOpen);
+popupCloseButton.addEventListener("click", popupClose);
 formElement.addEventListener("submit", handleFormSubmit);
+
+/*Кнопка для лайка*/
+/*let likeButtons = document.querySelectorAll(".place__button-like");
+
+for (let i = 0; i < likeButtons.length; i++) {
+  likeButtons[i].addEventListener("click", function () {
+    likeButtons[i].classList.toggle("place__button-like_active");
+  });
+}*/
